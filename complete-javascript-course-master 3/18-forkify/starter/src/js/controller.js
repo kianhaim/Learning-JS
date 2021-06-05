f10b75446f2638c6186a74f831c4be74730614d7
kianhaim <kianhaim@users.noreply.github.com>
2021-06-05 20:19:19 +0100

    json fetched

--- complete-javascript-course-master 3/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course-master 3/18-forkify/starter/src/js/controller.js
@@ -14,11 +14,26 @@ const timeout = function (s) {
 const showRecipe = async function () {
   try {
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=<insert your key>'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc96e'
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
 
     const data = await res.json();
-    console.log(res, data);
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    //console.log(res, data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
   } catch (err) {
     alert(err);
   }

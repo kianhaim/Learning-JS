f2c2eb388380958a6462cbdcc990112614c6c4dd
kianhaim <kianhaim@users.noreply.github.com>
2021-07-23 09:24:11 -0500

    Update script.js

--- complete-javascript-course-master 3/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master 3/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -398,18 +398,18 @@ console.log(10 / 3);
 
 // Create a date
 
-const now = new Date();
-console.log(now);
+// const now = new Date();
+// console.log(now);
 
-console.log(new Date('Aug 02 2020 18:05:41'));
-console.log(new Date('December 24, 2015'));
-console.log(new Date(account1.movementsDates[0]));
+// console.log(new Date('Aug 02 2020 18:05:41'));
+// console.log(new Date('December 24, 2015'));
+// console.log(new Date(account1.movementsDates[0]));
 
-console.log(new Date(2037, 10, 19, 15, 23, 5));
-console.log(new Date(2037, 10, 31));
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+// console.log(new Date(2037, 10, 31));
 
-console.log(new Date(0));
-console.log(new Date(3 * 24 * 60 * 60 * 1000));
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
 
 // Working with dates
 const future = new Date(2037, 10, 19, 15, 23);

bbbf1f49e1a1745d5e578f7d16a8ad3b5bea747e
kianhaim <kianhaim@users.noreply.github.com>
2021-07-28 12:18:33 -0500

    Update script.js

--- complete-javascript-course-master 3/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master 3/13-Advanced-DOM-Bankist/starter/script.js
@@ -19,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -30,3 +29,49 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////
+/////////////////////////////////////
+//////////////////////////////
+
+// Selecting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+const allSelector = document.querySelectorAll('.selector');
+console.log(allSelector);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+
+console.log(allButton);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting Elements
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class = "btn btn--close-cookie-message">Got It</button>';
+
+// header.prepend(message); // put message before header as a child
+
+header.append(message); // put message after the header as a child
+
+//header.append(message.cloneNode(true)); // put the message after the header as a child and clone it to appear on 2 places.
+
+// header.before(message); // put the message before the header as a sibling(not nested inside the header)
+// header.after(message); // put the message after the header as a sibling(not nested inside the header)
+
+// Delete Elements
+
+document
+  .querySelector('btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    // message.parentElement.removeChild(message); // old method
+  });

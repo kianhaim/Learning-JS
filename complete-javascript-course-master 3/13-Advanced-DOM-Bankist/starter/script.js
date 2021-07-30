32c887f05ee54da9e36e610f22f7c769e3cbe867
kianhaim <kianhaim@users.noreply.github.com>
2021-07-30 11:00:37 -0500

    Update script.js

--- complete-javascript-course-master 3/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master 3/13-Advanced-DOM-Bankist/starter/script.js
@@ -39,17 +39,17 @@ document.addEventListener('keydown', function (e) {
 // Button scrolling
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+  // console.log(s1coords);
 
-  console.log(e.target.getBoundingClientRect());
+  // console.log(e.target.getBoundingClientRect());
 
-  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
 
-  console.log(
-    'height/width viewport',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
+  // console.log(
+  //   'height/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
 
   // Scrolling
   // window.scrollTo(
@@ -88,7 +88,7 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   // Matching Stategy
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
-    console.log(id);
+    // console.log(id);
     document.querySelector(id).scrollIntoView({
       behavior: 'smooth',
     });

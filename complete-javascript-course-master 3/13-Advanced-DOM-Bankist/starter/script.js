68ebeba5ee499482f0b5563362528df08bd7dcaf
kianhaim <kianhaim@users.noreply.github.com>
2021-07-31 22:07:50 -0500

    Update script.js

--- complete-javascript-course-master 3/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master 3/13-Advanced-DOM-Bankist/starter/script.js
@@ -95,6 +95,46 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// Tabbed component
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+///////////////////////////////////////
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
 /////////////////////////////
 /////////////////////////////////////
 //////////////////////////////
@@ -258,7 +298,7 @@ document.querySelector('.nav').addEventListener(
   },
   true
 );
-*/
+
 
 // DOM TRAVERSING
 
@@ -295,3 +335,5 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
+
+*/

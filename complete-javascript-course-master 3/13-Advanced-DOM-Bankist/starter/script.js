ccbbb8dffb74c25407d108a00305b3bebf8a71c5
kianhaim <kianhaim@users.noreply.github.com>
2021-08-02 21:59:58 -0500

    Update script.js

--- complete-javascript-course-master 3/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master 3/13-Advanced-DOM-Bankist/starter/script.js
@@ -152,12 +152,36 @@ window.addEventListener('scroll', function (e) {
 });
 */
 // Sticky Navigation using Intersection Observer API
-const obsCallback = function () {};
-const obsOptions = {};
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
 
-const observer = new IntersectionObserver(obsCallback, obsOptions);
+///Sticky NAV using the header as a intection observer
+const navHeight = nav.getBoundingClientRect().height;
 
-observer.observe(section1);
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  // navi appears -90 px from the section
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
 /////////////////////////////
 /////////////////////////////////////
 //////////////////////////////

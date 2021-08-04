036475e41197269e1991c3392bbda6838d542671
kianhaim <kianhaim@users.noreply.github.com>
2021-08-03 22:04:53 -0500

    Update script.js

--- complete-javascript-course-master 3/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master 3/13-Advanced-DOM-Bankist/starter/script.js
@@ -188,7 +188,7 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
+  //console.log(entry);
   if (!entry.isIntersecting) return;
   else {
     entry.target.classList.remove('section--hidden');
@@ -206,6 +206,34 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+/// Lazy Loading images
+const imgTarget = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  ////replace src with data-src
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTarget.forEach(img => imgObserver.observe(img));
 /////////////////////////////
 /////////////////////////////////////
 //////////////////////////////

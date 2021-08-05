715af7cd18f765f0655fd145f2beb7471530bb11
kianhaim <kianhaim@users.noreply.github.com>
2021-08-05 14:19:27 -0500

    Update script.js

--- complete-javascript-course-master 3/14-OOP/starter/script.js
+++ complete-javascript-course-master 3/14-OOP/starter/script.js
@@ -6,9 +6,16 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 };
 
+// New {} is created
+// Function is call, this = {}
+//{} link to prototype
+// function automatically return {}
 const jonas = new Person('Jonas', 1991);
 const matilda = new Person('Matilda', 1997);
 const matt = new Person('Matt', 2017);
 console.log(jonas);
 console.log(matilda);
 console.log(matt);
+
+// True
+console.log(jonas instanceof Person);

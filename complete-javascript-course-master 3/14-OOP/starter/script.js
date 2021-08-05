60ff23cd742f76ed1a072c9eeff681c567211c90
kianhaim <kianhaim@users.noreply.github.com>
2021-08-05 11:04:15 -0500

    Update script.js

--- complete-javascript-course-master 3/14-OOP/starter/script.js
+++ complete-javascript-course-master 3/14-OOP/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  //Instance Properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person('Jonas', 1991);
+const matilda = new Person('Matilda', 1997);
+const matt = new Person('Matt', 2017);
+console.log(jonas);
+console.log(matilda);
+console.log(matt);

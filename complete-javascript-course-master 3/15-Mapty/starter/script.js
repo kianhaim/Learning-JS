801a4d83be6b41336edd424f995a282593b3a234
kianhaim <kianhaim@users.noreply.github.com>
2021-08-10 14:20:06 -0500

    Update script.js

--- complete-javascript-course-master 3/15-Mapty/starter/script.js
+++ complete-javascript-course-master 3/15-Mapty/starter/script.js
@@ -10,3 +10,10 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function () {},
+  function () {
+    alert(`Could not get your position.`);
+  }
+);

abd9636cd87e31b1e727314f9f818e36bd3b81ef
kianhaim <kianhaim@users.noreply.github.com>
2021-08-10 21:23:32 -0500

    Update script.js

--- complete-javascript-course-master 3/15-Mapty/starter/script.js
+++ complete-javascript-course-master 3/15-Mapty/starter/script.js
@@ -16,15 +16,16 @@ navigator.geolocation.getCurrentPosition(
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const coords = [latitude, longitude];
 
-    const map = L.map('map').setView([51.505, -0.09], 13);
+    const map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker([51.5, -0.09])
+    L.marker(coords)
       .addTo(map)
       .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
       .openPopup();

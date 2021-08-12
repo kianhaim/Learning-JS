289ba0683c9c2645ac8edc99906eae278a5136f4
kianhaim <kianhaim@users.noreply.github.com>
2021-08-11 22:26:21 -0500

    Update script.js

--- complete-javascript-course-master 3/15-Mapty/starter/script.js
+++ complete-javascript-course-master 3/15-Mapty/starter/script.js
@@ -23,12 +23,7 @@ class App {
 
     form.addEventListener('submit', this._newWorkout.bind(this));
 
-    inputType.addEventListener('change', function () {
-      inputElevation
-        .closest('.form__row')
-        .classList.toggle('form__row--hidden');
-      inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-    });
+    inputType.addEventListener('change', this._toggleElevationField.bind(this));
   }
 
   _getPosition() {
@@ -63,7 +58,10 @@ class App {
     inputDistance.focus();
   }
 
-  _toggleElevationField() {}
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
   _newWorkout(e) {
     e.preventDefault();

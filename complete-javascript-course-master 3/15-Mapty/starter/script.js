32c2f009ad1f0c0bd299aab016dd34560e3e888e
kianhaim <kianhaim@users.noreply.github.com>
2021-08-11 20:50:10 -0500

    Update script.js

--- complete-javascript-course-master 3/15-Mapty/starter/script.js
+++ complete-javascript-course-master 3/15-Mapty/starter/script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude } = position.coords;
@@ -18,14 +20,15 @@ navigator.geolocation.getCurrentPosition(
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
     const coords = [latitude, longitude];
 
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
-
-    map.on('click', function (mapEvent) {
+    ////////////   Handling clicks on Map ////////////////////
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
       form.classList.remove('hidden');
       inputDistance.focus();
 
@@ -36,10 +39,10 @@ navigator.geolocation.getCurrentPosition(
     alert(`Could not get your position.`);
   }
 );
-/////////////////////
 
-form.addEventListener('submit', function () {
-  //Display the marker
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  /////// Display the marker  /////////////////
   const { lat, lng } = mapEvent.latlng;
 
   L.marker([lat, lng])

ac3cb3e026d09001a1f30ae5e6a173e211838f61
kianhaim <kianhaim@users.noreply.github.com>
2021-08-12 10:12:14 -0500

    August 12, 2021

--- complete-javascript-course-master 3/15-Mapty/starter/script.js
+++ complete-javascript-course-master 3/15-Mapty/starter/script.js
@@ -11,8 +11,56 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+///////////// Work Out Class //////////////////
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
 
+  constructor(coords, distance, duration) {
+    this.coords = coords; // Arr [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in mins
+  }
+}
+
+///////////// Running Class //////////////////
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // Min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+/////////////// Cycling Class //////////////////
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km / h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+////// Examples /////////
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycle1 = new Cycling([39, -12], 5.2, 24, 178);
+
+// console.log(cycle1);
+// console.log(run1);
+
+/////////////////////////////////// Application Architecture Class //////////////////////////////////
 class App {
   /// Private Instance properties
   #map;

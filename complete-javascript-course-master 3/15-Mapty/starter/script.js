0051c64c2e17c9a5f21717892bd61811da544bf3
kianhaim <kianhaim@users.noreply.github.com>
2021-08-11 21:42:56 -0500

    Update script.js

--- complete-javascript-course-master 3/15-Mapty/starter/script.js
+++ complete-javascript-course-master 3/15-Mapty/starter/script.js
@@ -14,38 +14,45 @@ const inputElevation = document.querySelector('.form__input--elevation');
 let map, mapEvent;
 
 class App {
-  constructor() {}
+  /// Private Instance properties
+  #map;
+  #mapEvent;
+
+  constructor() {
+    this._getPosition();
+  }
+
   _getPosition() {
     if (navigator.geolocation)
       navigator.geolocation.getCurrentPosition(
-        function (position) {
-          const { latitude } = position.coords;
-          const { longitude } = position.coords;
-          console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-          const coords = [latitude, longitude];
-
-          map = L.map('map').setView(coords, 13);
-
-          L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-            attribution:
-              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-          }).addTo(map);
-          ////////////   Handling clicks on Map ////////////////////
-          map.on('click', function (mapE) {
-            mapEvent = mapE;
-            form.classList.remove('hidden');
-            inputDistance.focus();
-
-            //console.log(mapEvent);
-          });
-        },
+        this._loadMap.bind(this),
         function () {
           alert(`Could not get your position.`);
         }
       );
   }
 
-  _loadMap() {}
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const coords = [latitude, longitude];
+
+    this.#map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+    ////////////   Handling clicks on Map ////////////////////
+    this.#map.on('click', function (mapE) {
+      this.#mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+
+      //console.log(mapEvent);
+    });
+  }
 
   _showForm() {}
 
@@ -53,6 +60,8 @@ class App {
 
   _newWorkout() {}
 }
+
+const app = new App();
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();

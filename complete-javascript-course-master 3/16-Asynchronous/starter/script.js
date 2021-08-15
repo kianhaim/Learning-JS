9b9937ffca43cdc730fb952003afcfc6fd8c8ba4
kianhaim <kianhaim@users.noreply.github.com>
2021-08-15 18:44:21 -0500

    Update script.js

--- complete-javascript-course-master 3/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master 3/16-Asynchronous/starter/script.js
@@ -265,9 +265,17 @@ btn.addEventListener('click', function () {
   whereAmI(-33.933, 18.474);
 });
 
-*/
+///////////////////////// MicroTask Que
 
 console.log('Test Start');
 setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Resolve promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000; i++) {
+    console.log(res);
+  }
+});
+
 console.log('Test end');
+*/

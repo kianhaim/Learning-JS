086a654f80efe726d2d90909c9193281bde8d664
kianhaim <kianhaim@users.noreply.github.com>
2021-08-16 09:18:41 -0500

    Update script.js

--- complete-javascript-course-master 3/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master 3/16-Asynchronous/starter/script.js
@@ -471,31 +471,23 @@ const getPosition = function () {
 };
 
 const whereAmI = async function () {
-  try {
+  {
     // Geolocation
     const pos = await getPosition();
     const { latitude: lat, longitude: lng } = pos.coords;
 
     // Reverse geocoding
     const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    if (!resGeo.ok) throw new Error('Problem getting location data');
     const dataGeo = await resGeo.json();
+    console.log(dataGeo);
 
     // Country data
     const res = await fetch(
       `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
     );
-    if (!resGeo.ok) throw new Error('Problem getting country');
     const data = await res.json();
+    console.log(data);
     renderCountry(data[0]);
-
-    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
-  } catch (err) {
-    console.error(`${err} 💥`);
-    renderError(`💥 ${err.message}`);
-
-    // Reject promise returned from async function
-    throw err;
   }
 };
 

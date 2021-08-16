e3a505d290f6e8080aefeaf4e3dba2f63c1ed818
kianhaim <kianhaim@users.noreply.github.com>
2021-08-15 20:45:57 -0500

    Update script.js

--- complete-javascript-course-master 3/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master 3/16-Asynchronous/starter/script.js
@@ -402,12 +402,18 @@ GOOD LUCK 😀
 
 //////////////// Answer ////////////////////
 
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
 const imageContainer = document.querySelector('.images');
 
 const createImage = function (imgPath) {
   return new Promise(function (resolve, reject) {
-    const img = document.createImage('img');
-    img.src = imagPath;
+    const img = document.createElement('img');
+    img.src = imgPath;
 
     img.addEventListener('load', function () {
       imageContainer.append(img);
@@ -418,3 +424,30 @@ const createImage = function (imgPath) {
     });
   });
 };
+
+let currentImg;
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-3.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 3 loaded');
+    return wait(2);
+  })
+  .catch(err => console.error(err));

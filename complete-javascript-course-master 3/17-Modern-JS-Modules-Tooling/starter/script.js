0e04b3c18749d1c5e82c689a76ae212d2835c04c
kianhaim <kianhaim@users.noreply.github.com>
2021-08-20 11:00:00 -0600

    Update script.js

--- complete-javascript-course-master 3/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master 3/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,10 +1,14 @@
 'use strict';
 
 // Importing Module
-import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
+import {
+  addToCart,
+  totalPrice as price,
+  totalQuantity,
+} from './shoppingCart.js';
 
 console.log('Importing Module');
 
 addToCart('bread', 5);
 
-console.log(totalPrice, totalQuantity);
+console.log(price, totalQuantity);

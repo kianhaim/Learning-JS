f41410d3ed7ee9cde460cc05fd0071db46c43208
kianhaim <kianhaim@users.noreply.github.com>
2021-08-22 11:34:22 -0500

    Update script.js

--- complete-javascript-course-master 3/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master 3/01-Fundamentals-Part-1/starter/script.js
@@ -266,9 +266,9 @@ console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
 
 // GOOD LUCK 😀
 
-const bill = 430;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const bill = 430;
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-console.log(
-  `The bills was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
-);
+// console.log(
+//   `The bills was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+// );

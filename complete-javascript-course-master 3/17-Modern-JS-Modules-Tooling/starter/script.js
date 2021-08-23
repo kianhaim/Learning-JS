b9ba0c01ce096d8d7681a7f4503f3e89f5301759
kianhaim <kianhaim@users.noreply.github.com>
2021-08-23 08:11:21 -0500

    Update script.js

--- complete-javascript-course-master 3/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master 3/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -14,7 +14,7 @@ console.log('Importing Module');
 
 // Do not mix default and named exports
 //import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-
+/*
 import add, { cart } from './shoppingCart.js';
 add('pizza', 2);
 add('bread', 5);
@@ -22,3 +22,37 @@ add('orange', 10);
 add('apples', 12);
 
 console.log(cart);
+*/
+
+////////////// Old way to implement Modules /////////////////////////////
+
+const ShoppingCart2 = (function () {
+  const shippingCost = 10;
+  const cart = [];
+  const totalPrice = 230;
+  const totalQuantity = 10;
+
+  const addToCart = function (product, quantity) {
+    cart.push(product, quantity);
+    console.log(`${quantity} ${product}  added to the cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} order from supllier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalQuantity,
+    totalPrice,
+    orderStock,
+    //shippingCost,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 5);
+
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);

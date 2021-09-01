6312bc0867b1d6ef0abd8a35dcc79d0ccefc9462
kianhaim <kianhaim@users.noreply.github.com>
2021-09-01 08:44:50 -0500

    9/1

--- complete-javascript-course-master 3/18-forkify/starter/src/js/model.js
+++ complete-javascript-course-master 3/18-forkify/starter/src/js/model.js
@@ -28,7 +28,7 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-    console.log(state.recipe);
+    // console.log(state.recipe);
   } catch (err) {
     /// Temp Error Handling
     console.error(`${err}🎲!!!!!!!!!!!!!!!!`);
